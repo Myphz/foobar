b18e7c2ff9145a53bd92be2f20da427025f24272
Myphz <danielscanu45@outlook.it>
2020-10-10 13:21:07 +0200

    Initial commit

---
+++ Find Access Codes.py
@@ -0,0 +1,25 @@
+def solution(l):
+	tested = set()
+	result = {}
+	count = 0
+	length = len(l)
+	while length > 2:
+		divisors = findDivisors(l, l[-1], length - 1)
+		for divisor in divisors:
+			if (divisor[0], divisor[1]) in tested:
+				count += result[(divisor[0], divisor[1])]
+			else:
+				tested.add(tuple((divisor[0], divisor[1])))
+				result[(divisor[0], divisor[1])] = len(findDivisors(l, divisor[0], divisor[1]))
+				count += result[(divisor[0], divisor[1])]
+		l.pop(length - 1)
+		length -= 1
+	return count
+
+
+def findDivisors(l, e, j):
+	divisors = []
+	for i in range(j):
+		if e % l[i] == 0:
+			divisors.append((l[i], i))
+	return divisors
