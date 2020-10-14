969f263c2683ab76cf89bd10210a1d6618e38052
Daniel <71951166+Myphz@users.noreply.github.com>
2020-10-14 17:20:53 +0200

    Improved comments

--- Find Access Codes/solution.py
+++ Find Access Codes/solution.py
@@ -1,14 +1,16 @@
 def solution(l):
-	tested = set()	# making it a set so it's way faster searching elements in it
+	tested = set()
 	result = {}
 	count = 0
 	length = len(l)
 	while length > 2:
 		divisors = findDivisors(l, l[-1], length - 1)
 		for divisor in divisors:
-			if (divisor[0], divisor[1]) in tested: 			# if the divisor has already been tested before, it just picks the value from the dictionary.
+			# If the divisor has already been tested before (i.e, if it is in the "tested" set), it simply picks the value from the dictionary containing the result.
+			if (divisor[0], divisor[1]) in tested: 			
 				count += result[(divisor[0], divisor[1])]
-			else:											# else, it calculates it and adds it to the dictionary
+			# Else, it calculates the result and put it in the "tested" set
+			else:
 				tested.add(tuple((divisor[0], divisor[1])))
 				result[(divisor[0], divisor[1])] = len(findDivisors(l, divisor[0], divisor[1]))
 				count += result[(divisor[0], divisor[1])]
@@ -16,8 +18,8 @@ def solution(l):
 		length -= 1
 	return count
 
-
-def findDivisors(l, e, j):    # finds all of the divisors of e in the list l up to index j
+# This function returns a list containing all the divisors of a number (e) in a list (l) up to an index (j)
+def findDivisors(l, e, j):    
 	divisors = []
 	for i in range(j):
 		if e % l[i] == 0:
