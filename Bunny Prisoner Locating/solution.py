6052b622d809d9bacfb8ca0f47a7d38b562065c7
Daniel <71951166+Myphz@users.noreply.github.com>
2020-10-14 17:22:42 +0200

    Improved comments

--- Bunny Prisoner Locating/solution.py
+++ Bunny Prisoner Locating/solution.py
@@ -1,12 +1,14 @@
-# Definitely not the best solution, as it calculates all of the combinations up to the last one, and just returns the last one.
+# Definitely not the best solution, as it calculates all of the sequence numbers up to the last one, and just returns the last one.
 # Anyway, it works and it's fast enough because the numbers are pretty small
+
+# This function is useful to determine the starting point of the sequence, given the height (n).
 def getStart(n):
     s = 1
     for i in range(0,n):
         s += i
     return s
 
-
+# It calculates every single value of the sequence and returns the last one.
 def solution(x, y):
     l = [getStart(y)]
     for i in range(2,x + 1):
