360443ae189ea0c8b6193c8a170db3d9f5410e74
Daniel <71951166+Myphz@users.noreply.github.com>
2020-10-15 11:39:55 +0200

    Update solution.py

--- Bomb, Baby!/solution.py
+++ Bomb, Baby!/solution.py
@@ -1,16 +1,16 @@
 def solution(x,y):
-	s = 0
-	x, y = int(x), int(y)
-	if x == y: return "impossible"
-	while (x,y) != (1,1) and x > 0 and y > 0:
-		if x > y: 
-			a = x//y
-			if a == x: a -= 1
-			x -= y * a
-			s += a
-		else:
-			a = y//x
-			if a == y: a -= 1		 
-			y -= x * a
-			s += a
-	return str(s) if (x,y) == (1,1) else "impossible"
+    s = 0
+    x, y = int(x), int(y)
+    if x == y: return "impossible"
+    while (x,y) != (1,1) and x > 0 and y > 0:
+        if x > y: 
+	    a = x//y
+	    if a == x: a -= 1
+	        x -= y * a
+		s += a
+	else:
+	    a = y//x
+	    if a == y: a -= 1		 
+	    y -= x * a
+	    s += a
+    return str(s) if (x,y) == (1,1) else "impossible"
